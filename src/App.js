5ef59d78bcd53488272ba0e4f1f34a36dc9509f5
kenkz447 <kenkz447@gmail.com>
2018-11-11 23:32:39 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,76 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+const XLSX = window['XLSX']
 class App extends Component {
+  state = {}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <textarea
+            value={this.state.content}
+            onChange={(e) => this.setState({ content: e.target.value })}
+          />
+        </div>
+        <button onClick={this.click}>X</button>
       </div>
     );
+  }
+
+  click = () => {
+    const json = this.parserJson()
+    console.log(json);
+
+    var ws = XLSX.utils.json_to_sheet(json, { header: ["Họ tên", "Ngày sinh", "Địa chỉ", "Điện thoại", "Email", "Nội dung"] });
+
+    var wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'xxx');
+
+    window['XLSX'].writeFile(wb, 'test.xlsx');
+  };
+
+  groupTexts = () => {
+    const { content } = this.state;
+    const stringAreas = []
+    let lastStringArea = []
+
+    var lines = content.split('\n');
+    for (var i = 0; i < lines.length; i++) {
+      if (lines[i]) {
+        lastStringArea[lastStringArea.length] = lines[i]
+      } else {
+        if (lastStringArea.length) {
+          stringAreas.push([...lastStringArea])
+          lastStringArea = []
+        }
+      }
+    }
+    return stringAreas
+  }
+
+  parserJson = () => {
+    const groupTexts = this.groupTexts()
+    const jsons = []
+
+    for (const groupText of groupTexts) {
+      const json = {}
+      for (let index = 0; index < groupText.length; index++) {
+        const text = groupText[index];
+        const key = text.split(':')[0]
+        const value = text.split(':')[1]
+
+        let keyParsered = key.replace('-', '').trim()
+        if (keyParsered === 'Tiêu đề') {
+          keyParsered = "Nội dung"
+        }
+        
+        json[keyParsered] = value
+      }
+      jsons.push(json)
+    }
+    return jsons;
   }
 }
 
